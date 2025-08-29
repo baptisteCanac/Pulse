e2be1324c1ce4127ecf663f28e9a1484c62bb24c
Baptiste Canac <baptiste0406@gmail.com>
2025-08-29 16:00:13 +0200

    simplification for the ColorMode class

--- Pulse/src/JsUtils/ColorMode.js
+++ Pulse/src/JsUtils/ColorMode.js
@@ -1,6 +1,9 @@
 export default class ColorMode{
     constructor(){
-        console.log("Color mode class initialised");
+        this.sidebar = document.querySelector("aside");
+        this.footer = this.sidebar.querySelector("div.mt-6");
+        this.mainCard = document.querySelector("main > div");
+        this.button = document.getElementById("openFileButton");
     }
 
     lightMode() {
@@ -10,21 +13,17 @@ export default class ColorMode{
         document.body.classList.remove("bg-gray-900", "text-white");
         document.body.classList.add("bg-white", "text-gray-900");
 
-        const sidebar = document.querySelector("aside");
-        sidebar.classList.remove("bg-gray-800");
-        sidebar.classList.add("bg-gray-100");
+        this.sidebar.classList.remove("bg-gray-800");
+        this.sidebar.classList.add("bg-gray-100");
 
-        const footer = sidebar.querySelector("div.mt-6");
-        footer.classList.remove("text-gray-400");
-        footer.classList.add("text-gray-600");
+        this.footer.classList.remove("text-gray-400");
+        this.footer.classList.add("text-gray-600");
 
-        const mainCard = document.querySelector("main > div");
-        mainCard.classList.remove("bg-gray-800");
-        mainCard.classList.add("bg-gray-50");
+        this.mainCard.classList.remove("bg-gray-800");
+        this.mainCard.classList.add("bg-gray-50");
 
-        const button = document.getElementById("openFileButton");
-        button.classList.remove("bg-blue-600", "text-white");
-        button.classList.add("bg-blue-500", "text-white");
+        this.button.classList.remove("bg-blue-600", "text-white");
+        this.button.classList.add("bg-blue-500", "text-white");
 
         // Changer le style des recent files en mode clair
         const recentFileElements = document.querySelectorAll(".bg-gray-700");
@@ -42,21 +41,17 @@ export default class ColorMode{
         document.body.classList.remove("bg-white", "text-gray-900");
         document.body.classList.add("bg-gray-900", "text-white");
 
-        const sidebar = document.querySelector("aside");
-        sidebar.classList.remove("bg-gray-100");
-        sidebar.classList.add("bg-gray-800");
+        this.sidebar.classList.remove("bg-gray-100");
+        this.sidebar.classList.add("bg-gray-800");
 
-        const footer = sidebar.querySelector("div.mt-6");
-        footer.classList.remove("text-gray-600");
-        footer.classList.add("text-gray-400");
+        this.footer.classList.remove("text-gray-600");
+        this.footer.classList.add("text-gray-400");
 
-        const mainCard = document.querySelector("main > div");
-        mainCard.classList.remove("bg-gray-50");
-        mainCard.classList.add("bg-gray-800");
+        this.mainCard.classList.remove("bg-gray-50");
+        this.mainCard.classList.add("bg-gray-800");
 
-        const button = document.getElementById("openFileButton");
-        button.classList.remove("bg-blue-500", "text-white");
-        button.classList.add("bg-blue-600", "text-white");
+        this.button.classList.remove("bg-blue-500", "text-white");
+        this.button.classList.add("bg-blue-600", "text-white");
 
         // Changer le style des recent files en mode sombre
         const recentFileElements = document.querySelectorAll(".bg-gray-700");
