72d52f81df2b65b79507f3107929eed7811e406f
Baptiste Canac <baptiste0406@gmail.com>
2025-09-11 18:49:02 +0200

    change language using python

--- Pulse/src/scripts/save_new_language.py
+++ Pulse/src/scripts/save_new_language.py
@@ -1,5 +1,17 @@
 import sys
+import json
 
 choosen_language = sys.argv[1]
+json_path = "../src/datas/languages.json"
 
-print(choosen_language)
+with open(json_path, "r", encoding="utf-8") as file:
+    data = json.load(file)
+
+# 2. Modifier la valeur souhaitée
+data["choosen_language"] = int(choosen_language)  # par exemple, changer à 0
+
+# 3. Écrire à nouveau dans le fichier JSON
+with open(json_path, "w", encoding="utf-8") as file:
+    json.dump(data, file, indent=4, ensure_ascii=False)
+
+print("Ok")
