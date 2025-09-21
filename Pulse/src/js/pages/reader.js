4970224e50e54fd6df1c4ff94acfd758cd5144f6
Baptiste Canac <baptiste0406@gmail.com>
2025-09-21 15:50:47 +0200

    basic visualisation of text

---
+++ Pulse/src/js/pages/reader.js
@@ -0,0 +1,17 @@
+const { invoke } = window.__TAURI__.core;
+
+import MarkdownParser from "../lib/MarkdownParser.js";
+import ColorMode from "../lib/ColorMode.js";
+
+async function codeTraitement(){
+    const markdownParser = new MarkdownParser(invoke);
+    const container = document.getElementById("container");
+
+    let code = await invoke("get_code");
+    let presentationPath = await invoke("get_presentation_path");
+    code = await markdownParser.parseAll(code, presentationPath);
+    
+    container.innerHTML = code;
+}
+
+codeTraitement();
