8d2e6fe517f6918ef852064dade56fd738943a03
Baptiste Canac <baptiste0406@gmail.com>
2025-09-22 11:56:51 +0200

    create a method for every page. this method call init. Use it into settings

--- Pulse/src/js/lib/HeadManager.js
+++ Pulse/src/js/lib/HeadManager.js
@@ -1,35 +1,45 @@
 import JsonManager from "./JsonManager.js";
 
-export default class HeadManager{
-    constructor(){
-        this.config;
+export default class HeadManager {
+    constructor() {
+        this.config = {};
         this.head = document.head;
     }
 
-    createElement(elementName, attributes){
+    createElement(elementName, attributes = {}) {
         const element = document.createElement(elementName);
-        if (Object.keys(attributes).length != 0){
-            for (const key in attributes){
-                element.setAttribute(key, attributes[key]);
-            }
+        for (const key in attributes) {
+            element.setAttribute(key, attributes[key]);
         }
         return element;
     }
 
-    async init(){
-        /*
-        Load global config
-        */
-        this.config = await new JsonManager("../../datas/config.json").getConfig();
-        this.config = this.config.headManager;
+    async init() {
+        // Charge définitivement la config
+        const fullConfig = await new JsonManager("../../datas/config.json").getConfig();
+        this.config = fullConfig.headManager; // mise à jour définitive
 
+        // Traitement des éléments globaux
         const global = this.config.global;
         const tags = global.tags;
         const attributes = global.attributes;
 
-        for (const tag in tags){
+        for (const tag in tags) {
             const element = this.createElement(tags[tag], attributes[tag]);
-            this.head.prepend(element);
+            this.head.appendChild(element);
+        }
+    }
+
+    async setHead(sectionName) {
+        await this.init();
+        
+        const container = this.config[sectionName];
+        const tags = container.tags;
+        const attributes = container.attributes;
+
+        for (const tag in tags) {
+            const element = this.createElement(tags[tag], attributes[tag]);
+            this.head.appendChild(element);
         }
     }
 }
