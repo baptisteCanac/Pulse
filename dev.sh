85f26f284489fd1867700f433cbf1f62ba90525f
Baptiste Canac <baptiste0406@gmail.com>
2025-10-20 16:58:57 +0200

    adding launcher
    Adding a launcher for a pro use during the dev. Use with the launcher to use it in conferences while the package is not build and launch

--- dev.sh
+++ dev.sh
@@ -1,5 +1,8 @@
-echo "Launching Pulse"
+#!/bin/bash
 
-cd Pulse
+# Ajouter le PATH pour npm et node
+export PATH="/opt/homebrew/bin:/usr/local/bin:$PATH"
 
+echo "Running Pulse dev"
+cd Pulse || exit 1
 npm run tauri dev
